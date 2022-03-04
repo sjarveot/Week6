3c622456b93dd9c1c862510fa7defefb5be0dd43
Siim Järveots <siim.jarveots@verston.ee>
2022-03-04 23:10:36 +0200

    Create StringArrays programm

---
+++ StringandArrays/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StringandArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada eesnimi;
+            //programm küsib kasutaja eesnime pikkust;
+
+            Console.WriteLine("Sisesta palun oma eesnimi");
+            string firstName = Console.ReadLine();
+
+            int firstNameLength = firstName.Length;
+            Console.WriteLine($"Sinu eesnimes on {firstNameLength} sümbolit.");
+
+        }
+    }
+}
